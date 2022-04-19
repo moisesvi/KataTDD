389f369df9491119421fa55fe5c8638375b0599d
moisesvi <71398390@continental.edu.pe>
2022-04-19 15:40:50 -0500

    Calculo promedio de todos los casos (verde)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -1,6 +1,15 @@
-class Conjunto():
-    def __int__(self, conjunto):
+class Conjunto:
+    def __init__(self, conjunto):
         self.__conjunto = conjunto
 
     def promedio(self):
-        return False
+        if len(self.__conjunto) == 1:
+            return self.__conjunto[0]
+        elif len(self.__conjunto) == 2:
+            return (self.__conjunto[0] + self.__conjunto[1]) / 2
+        elif len(self.__conjunto) > 2:
+            return sum(self.__conjunto) / len(self.__conjunto)
+        else:
+            return None
+
+
